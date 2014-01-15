5e8351d339ec2a54bf94e972ece26ca8f310165c
Christian Artin <gravufo@gmail.com>
2014-01-15 14:52:02 -0500

    Add all the test files as well as the bubble sort

---
+++ tp1/src/bubble/Bubble.java
@@ -0,0 +1,34 @@
+package tp1;
+
+import java.util.ArrayList;
+
+public class Bubble
+{
+	// Selon le pseudo code trouvé sur http://en.wikipedia.org/wiki/Bubble_sort
+	public ArrayList<Integer> sort(ArrayList<Integer> values)
+	{
+		// Détermine si un interchangement a eu lieu dans la dernière itération
+		boolean swapped = false;
+		   
+		do
+		{
+			swapped = false;
+			for(int i = 1; i < values.size(); i++)
+			{
+				// Si la paire n'est pas ordonnée
+				if(values.get(i-1) > values.get(i))
+				{
+					// on interchange les valeurs
+					int temp = values.get(i);
+					values.set(i, values.get(i-1));
+					values.set(i-1, temp);
+					swapped = true;
+				}
+			}
+		}
+		while(swapped);
+		
+		return values;
+	}
+	
+}
