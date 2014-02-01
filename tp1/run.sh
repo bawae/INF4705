322638799d0f44cbd0b6919ff809bcc07cd10f21
Christian Artin <gravufo@gmail.com>
2014-02-01 01:08:00 -0500

    Avancement relativement mineur du scripte de seuil

--- tp1/run.sh
+++ tp1/run.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
+FILE_NAME="seuil.csv"
+FILE_LOCATION="logs"
+TEST_FILES="./tp1-H10-donnees/*.txt"
+SORT_ARRAY=('quicksortmedseuil', 'quicksortseuil')
+SORT_ARGS="-f $FILE -p"
 
-echo "Nom du fichier,Algorithme,Seuil,Temps d'execution"
+# Déclarer les colonnes du tableau
+echo -e "Nom du fichier,Algorithme,Seuil,Temps d'execution" > $FILE_LOCATION/$FILE_NAME
 
-for(tous les fichiers)
-	for(seuil de 2 a 20)
-		appeler tous les sort > sortie.txt
+for f in $TEST_FILES
+do
+	for i=2 to 100
+	do
+		for f in $SORT_ARRAY
+		do
+			java -jar $f $SORT_ARGS > $FILE_LOCATION/$FILE_NAME
+		done
+	done
+done
