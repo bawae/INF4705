db7533873bb30b470e4c9f829cc7be96e7366abf
gabriellebourdages <gabrielle.bourdages@gmail.com>
2014-02-04 18:35:08 -0500

    analyse
    changement des arguments d'entrée pour les exécutables

--- tp1/analyse.sh
+++ tp1/analyse.sh
@@ -4,7 +4,6 @@ FILE_NAME="analyse.csv"
 FILE_LOCATION="logs"
 TEST_FILES="./tp1-H10-donnees/*.txt"
 SORT_ARRAY=('bubble' 'counting' 'quick' 'quickMed' 'quickSeuil' 'quickMedSeuil')
-SORT_ARGS="-f $FILE"
 
 # Déclarer les colonnes du tableau
 echo "Nom du fichier,bubble,counting,quick,quickMed,quickSeuil,quickMedSeuil \n" > $FILE_LOCATION/$FILE_NAME
@@ -14,7 +13,7 @@ do
 	echo "$f,"
 	for a in $SORT_ARRAY
 	do
-		java -jar $a "$SORT_ARGS," > $FILE_LOCATION/$FILE_NAME
+		java -jar $a "-f $f" > $FILE_LOCATION/$FILE_NAME
 	done
 	echo "\n"
 done
