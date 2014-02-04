1c7296fe78fbd9e63167a171392a2be8473ffeff
gabriellebourdages <gabrielle.bourdages@gmail.com>
2014-02-04 18:17:35 -0500

    quick modif
    maiwmiamwiamwimiaw

--- tp1/analyse.sh
+++ tp1/analyse.sh
@@ -3,19 +3,18 @@
 FILE_NAME="analyse.csv"
 FILE_LOCATION="logs"
 TEST_FILES="./tp1-H10-donnees/*.txt"
-SORT_ARRAY=('bubble','counting','quick','quickMed','quickSeuil', 'quickMedSeuil')
+SORT_ARRAY=('bubble' 'counting' 'quick' 'quickMed' 'quickSeuil' 'quickMedSeuil')
 SORT_ARGS="-f $FILE"
 
 # Déclarer les colonnes du tableau
-echo -e "Nom du fichier,bubble,counting,quick,quickMed,quickSeuil,quickMedSeuil" > $FILE_LOCATION/$FILE_NAME
+echo "Nom du fichier,bubble,counting,quick,quickMed,quickSeuil,quickMedSeuil \n" > $FILE_LOCATION/$FILE_NAME
 
 for f in $TEST_FILES
 do
 	echo "$f,"
 	for a in $SORT_ARRAY
 	do
-		java -jar $f $SORT_ARGS > $FILE_LOCATION/$FILE_NAME
-		echo ","
+		java -jar $a "$SORT_ARGS," > $FILE_LOCATION/$FILE_NAME
 	done
 	echo "\n"
 done
