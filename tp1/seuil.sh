994999c80a54c9d2f9f30d5fa147fe8eadb1e108
Christian Artin <gravufo@gmail.com>
2014-02-04 23:26:28 -0500

    logs, yay

--- tp1/seuil.sh
+++ tp1/seuil.sh
@@ -2,7 +2,7 @@
 FILE_NAME='seuil.csv'
 FILE_LOCATION='logs'
 TEST_FILES_LOCATION='tp1-H10-donnees'
-TEST_FILE_ARRAY=(1000 5000 10000 50000 100000)
+TEST_FILE_ARRAY=(1000 5000 10000) # 50000 100000)
 SORT_ARRAY=(quickSeuil quickMedSeuil)
 
 # Déclarer les colonnes du tableau
@@ -12,13 +12,13 @@ for f in ${TEST_FILE_ARRAY[@]}
 do
 	TEST_FILE_NAME="testset_"$f"_0.txt"
 
-	for((i=2; i <= 100; i++))
+	for((i=5; i <= 50; i++))
 	do
-#		SORT_ARGS="-f ""$TEST_FILES_LOCATION""/""$TEST_FILE_NAME"" -s ""$i"
+#		SORT_ARGS="-f ""$TEST_FILES_LOCATION""/""$TEST_FILE_NAME"" -s ""$i
 
 		for sort in ${SORT_ARRAY[@]}
 		do
-			echo $TEST_FILE_NAME","$sort","$i"," >> $FILE_LOCATION/$FILE_NAME
+			echo -n $TEST_FILE_NAME,$sort,$i, >> $FILE_LOCATION/$FILE_NAME
 #			echo -e $sort.jar $SORT_ARGS
 			java -jar $sort.jar -f $TEST_FILES_LOCATION/$TEST_FILE_NAME -s $i >> $FILE_LOCATION/$FILE_NAME
 		done
