1c1e5099c61e99152fb15239f116a991976e0ff2
Christian Artin <gravufo@gmail.com>
2014-02-04 22:04:16 -0500

    fix du script analyse.sh

--- tp1/analyse.sh
+++ tp1/analyse.sh
@@ -6,14 +6,16 @@ TEST_FILES_LOCATION='/tp1-H10-donnees/'
 SORT_ARRAY=( bubble counting quick quickMed quickSeuil quickMedSeuil )
 SORT_ARRAY_ELEMENTS=${#SORT_ARRAY[@]}
 
-echo "Nom du fichier,bubble,counting,quick,quickMed,quickSeuil,quickMedSeuil \n" > $FILE_LOCATION/$FILE_NAME
+echo -e "Nom du fichier,bubble,counting,quick,quickMed,quickSeuil,quickMedSeuil" > $FILE_LOCATION/$FILE_NAME
 
 for f in $TEST_FILES_LOCATION/*.txt
 do
 	echo $f"," >> $FILE_LOCATION/$FILE_NAME
-	for ((j=0;j<SORT_ARRAY_ELEMENTS;j++));
+
+	for ((j=0;j<SORT_ARRAY_ELEMENTS;j++))
 	do
-		java -jar $SORT_ARRAY[j].jar $f >> $FILE_LOCATION/$FILE_NAME
+		java -jar ${SORT_ARRAY[j]}.jar $f >> $FILE_LOCATION/$FILE_NAME
 	done
-	echo "\n" >> $FILE_LOCATION/$FILE_NAME
+
+	echo -e "" >> $FILE_LOCATION/$FILE_NAME
 done
