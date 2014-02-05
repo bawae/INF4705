172146a42840795d131b5bc703a80aa35b1c955c
Christian Artin <gravufo@dwellers-server.kiki.kicks-ass.org>
2014-02-04 21:36:15 -0500

    still this one single error...

--- tp1/seuil.sh
+++ tp1/seuil.sh
@@ -4,7 +4,6 @@ FILE_LOCATION='logs'
 TEST_FILES_LOCATION='tp1-H10-donnees'
 TEST_FILE_ARRAY=(1000 5000 10000 50000 100000)
 SORT_ARRAY=('quickSeuil' 'quickMedSeuil')
-SORT_ARGS="-f ""$TEST_FILES_LOCATION""/""$TEST_FILE_NAME"" -s"
 
 # Déclarer les colonnes du tableau
 echo -e "Nom du fichier,Algorithme,Seuil,Temps d'execution" > "$FILE_LOCATION""/""$FILE_NAME"
@@ -13,13 +12,15 @@ for f in "${TEST_FILE_ARRAY[@]}"
 do
 	TEST_FILE_NAME="testset_""$f""_0.txt"
 
-	for((i=2; i <= 100; i))
+	for((i=2; i <= 100; i++))
 	do
+		SORT_ARGS="-f ""$TEST_FILES_LOCATION""/""$TEST_FILE_NAME"" -s ""$i"
+
 		for sort in "${SORT_ARRAY[@]}"
 		do
-			echo "$TEST_FILE_NAME,$sort,$i," >> $FILE_LOCATION/$FILE_NAME
-			echo -e "$sort"".jar" "$SORT_ARGS" "$i"
-			java -jar "$sort"".jar" "$SORT_ARGS" "$i" >> $FILE_LOCATION/$FILE_NAME
+			echo "$TEST_FILE_NAME"",""$sort"",""$i", >> "$FILE_LOCATION""/""$FILE_NAME"
+			echo -e "$sort"".jar ""$SORT_ARGS"
+#			"java -jar ""$sort"".jar ""$SORT_ARGS" >> "$FILE_LOCATION""/""$FILE_NAME"
 		done
 	done
 done
