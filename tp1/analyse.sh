ff496a99aec6aa4ffb158627f33fe004d1dc7011
Christian Artin <gravufo@gmail.com>
2014-02-04 22:20:01 -0500

    Fixed script, now works!

--- tp1/analyse.sh
+++ tp1/analyse.sh
@@ -2,19 +2,19 @@
 
 FILE_NAME="analyse.csv"
 FILE_LOCATION="logs"
-TEST_FILES_LOCATION='/tp1-H10-donnees/'
+TEST_FILES_LOCATION='tp1-H10-donnees'
 SORT_ARRAY=( bubble counting quick quickMed quickSeuil quickMedSeuil )
 SORT_ARRAY_ELEMENTS=${#SORT_ARRAY[@]}
 
 echo -e "Nom du fichier,bubble,counting,quick,quickMed,quickSeuil,quickMedSeuil" > $FILE_LOCATION/$FILE_NAME
 
-for f in $TEST_FILES_LOCATION/*.txt
+for f in $TEST_FILES_LOCATION/*
 do
 	echo $f"," >> $FILE_LOCATION/$FILE_NAME
 
 	for ((j=0;j<SORT_ARRAY_ELEMENTS;j++))
 	do
-		java -jar ${SORT_ARRAY[j]}.jar $f >> $FILE_LOCATION/$FILE_NAME
+		java -jar "${SORT_ARRAY[j]}".jar -f $f >> $FILE_LOCATION/$FILE_NAME
 	done
 
 	echo -e "" >> $FILE_LOCATION/$FILE_NAME
