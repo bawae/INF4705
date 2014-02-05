ac0b9ecaa91e4dcb1a7b09a6bd2e894858434d72
gabriellebourdages <gabrielle.bourdages@gmail.com>
2014-02-04 21:52:36 -0500

    no file array lel

--- tp1/analyse.sh
+++ tp1/analyse.sh
@@ -2,19 +2,18 @@
 
 FILE_NAME="analyse.csv"
 FILE_LOCATION="logs"
-TEST_FILES="./tp1-H10-donnees/*.txt"
+TEST_FILES_LOCATION='/tp1-H10-donnees/'
 SORT_ARRAY=( bubble counting quick quickMed quickSeuil quickMedSeuil )
 SORT_ARRAY_ELEMENTS=${#SORT_ARRAY[@]}
-TEST_FILES_ELEMENTS=${#TEST_FILES[@]}
 
 echo "Nom du fichier,bubble,counting,quick,quickMed,quickSeuil,quickMedSeuil \n" > $FILE_LOCATION/$FILE_NAME
 
-for ((i=0;i<TEST_FILES_ELEMENTS;i++));
+for f in $TEST_FILES_LOCATION/*.txt
 do
-	echo $TEST_FILES[i]"," >> $FILE_LOCATION/$FILE_NAME
+	echo $f"," >> $FILE_LOCATION/$FILE_NAME
 	for ((j=0;j<SORT_ARRAY_ELEMENTS;j++));
 	do
-		java -jar $SORT_ARRAY[j].jar $TEST_FILES[i] >> $FILE_LOCATION/$FILE_NAME
+		java -jar $SORT_ARRAY[j].jar $f >> $FILE_LOCATION/$FILE_NAME
 	done
 	echo "\n" >> $FILE_LOCATION/$FILE_NAME
 done
