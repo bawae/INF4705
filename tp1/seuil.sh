bd9d70c1fc3b5f87a74af04d231a7c4033e92b52
Christian Artin <gravufo@gmail.com>
2014-02-07 01:23:15 -0500

    Changed script and added log file for seuil

--- tp1/seuil.sh
+++ tp1/seuil.sh
@@ -2,7 +2,7 @@
 FILE_NAME='seuil.csv'
 FILE_LOCATION='logs'
 TEST_FILES_LOCATION='tp1-H10-donnees'
-TEST_FILE_ARRAY=(1000 5000 10000) # 50000 100000)
+TEST_FILE_ARRAY=(1000 5000 10000 50000 100000)
 SORT_ARRAY=(quickSeuil quickMedSeuil)
 
 # Déclarer les colonnes du tableau
@@ -10,17 +10,20 @@ echo -e "Nom du fichier,Algorithme,Seuil,Temps d'execution" > $FILE_LOCATION/$FI
 
 for f in ${TEST_FILE_ARRAY[@]}
 do
-	TEST_FILE_NAME="testset_"$f"_0.txt"
-
-	for((i=5; i <= 50; i++))
+	for((j=0; j < 30; j++))
 	do
-#		SORT_ARGS="-f ""$TEST_FILES_LOCATION""/""$TEST_FILE_NAME"" -s ""$i
+		TEST_FILE_NAME="testset_"$f"_"$j".txt"
 
-		for sort in ${SORT_ARRAY[@]}
+		for((i=5; i <= 50; i++))
 		do
-			echo -n $TEST_FILE_NAME,$sort,$i, >> $FILE_LOCATION/$FILE_NAME
-#			echo -e $sort.jar $SORT_ARGS
-			java -jar $sort.jar -f $TEST_FILES_LOCATION/$TEST_FILE_NAME -s $i >> $FILE_LOCATION/$FILE_NAME
+	#		SORT_ARGS="-f ""$TEST_FILES_LOCATION""/""$TEST_FILE_NAME"" -s ""$i
+
+			for sort in ${SORT_ARRAY[@]}
+			do
+				echo -n $TEST_FILE_NAME,$sort,$i, >> $FILE_LOCATION/$FILE_NAME
+	#			echo -e $sort.jar $SORT_ARGS
+				java -jar $sort.jar -f $TEST_FILES_LOCATION/$TEST_FILE_NAME -s $i >> $FILE_LOCATION/$FILE_NAME
+			done
 		done
 	done
 done
