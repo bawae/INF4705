1eccba5e6fe31ecf372a8bdf7c5cfe66432774ba
Gabrielle Bourdages <gabrielle.bourdages@gmail.com>
2014-03-16 15:57:58 -0400

    analyse.sh

---
+++ tp2/analyse.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+FILE_NAME="analyse.csv"
+FILE_LOCATION="logs"
+TEST_FILES_LOCATION='Restos'
+SORT_ARRAY=( vorace dynamique local )
+SORT_ARRAY_ELEMENTS=${#SORT_ARRAY[@]}
+
+echo -e "Nom du fichier,vorace,dynamique,local" > $FILE_LOCATION/$FILE_NAME
+
+for f in $TEST_FILES_LOCATION/*
+do
+	echo -n $f >> $FILE_LOCATION/$FILE_NAME
+
+	for ((j=0;j<SORT_ARRAY_ELEMENTS;j++))
+	do
+		echo -n "," >> $FILE_LOCATION/$FILE_NAME
+		java -jar ${SORT_ARRAY[j]}.jar -f $f -s 30 >> $FILE_LOCATION/$FILE_NAME
+	done
+
+	echo -e "" >> $FILE_LOCATION/$FILE_NAME
+done
